b9d4865ee0b10b402f4137ea64d684e4bd527488
Rancho <ranchocooper@gmail.com>
2020-01-10 23:52:29 +0800

    high concurrency with worker poll

---
+++ high-concurrency/wait-group.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+func main() {
+	wg := sync.WaitGroup{}
+	
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+		go calc(&wg, i)
+		wg.Done()
+	}
+	
+	wg.Wait()
+	fmt.Println("all goroutine finish")
+}
+
+func calc(w *sync.WaitGroup, i int) {
+	
+	fmt.Println("calc:", i)
+	time.Sleep(time.Second)
+}
+
