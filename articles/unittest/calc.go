6231f7eac9d7cbd3f2324a0fa1d4a314bcf2b25e
Rancho <ranchocooper@gmail.com>
2020-06-16 19:29:51 +0800

    up

---
+++ articles/unittest/calc.go
@@ -0,0 +1,10 @@
+package unittest
+
+//https://geektutu.com/post/quick-go-test.html
+func Add(a int, b int) int {
+	return a + b
+}
+
+func Mul(a int, b int) int {
+	return a * b
+}
---
+++ articles/unittest/calc_test.go
@@ -0,0 +1,55 @@
+package unittest
+
+import (
+	"fmt"
+	"os"
+	"testing"
+)
+
+func TestAdd(t *testing.T) {
+	if ans := Add(1, 2); ans != 3 {
+		t.Errorf("1 + 2 expected be 3, but %d got", ans)
+	}
+
+	t.Helper()
+	if ans := Add(-10, -20); ans != -30 {
+		t.Errorf("-10 + -20 expected be -30, but %d got", ans)
+	}
+}
+
+func TestMul(t *testing.T) {
+	t.Run("pos", func(t *testing.T) {
+		if Mul(2, 3) != 6 {
+			t.Fatal("fail")
+		}
+
+	})
+	t.Run("neg", func(t *testing.T) {
+		if Mul(2, -3) != -6 {
+			t.Fatal("fail")
+		}
+	})
+}
+
+func setup() {
+	fmt.Println("Before all tests")
+}
+
+func teardown() {
+	fmt.Println("After all tests")
+}
+
+func Test1(t *testing.T) {
+	fmt.Println("I'm test1")
+}
+
+func Test2(t *testing.T) {
+	fmt.Println("I'm test2")
+}
+
+func TestMain(m *testing.M) {
+	setup()
+	code := m.Run()
+	teardown()
+	os.Exit(code)
+}
