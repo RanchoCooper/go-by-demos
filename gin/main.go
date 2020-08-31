26ddde8d023adcb9af3f25d010cf6cd3bcf14af6
Rancho <ranchocooper@gmail.com>
2020-09-01 00:28:50 +0800

    gin: run a server

--- gin/main.go
+++ gin/main.go
@@ -1,63 +1,25 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
+	"fmt"
 	"log"
-	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	// 初始化引擎
+	// create gin engine with default configs
 	engine := gin.Default()
 
-	// 注册路由和处理函数
-	engine.Any("/", WebRoot)
-	// 其他注册方式
-	engine.Handle("GET", "/get", WebRoot)
-	engine.GET("/someGet", WebRoot)
-	engine.POST("/somePost", WebRoot)
-	engine.PUT("/somePut", WebRoot)
-	engine.DELETE("/someDelete", WebRoot)
-	engine.PATCH("/somePatch", WebRoot)
-	engine.HEAD("/someHead", WebRoot)
-	engine.OPTIONS("/someOptions", WebRoot)
-
-	// 动态路由
-	engine.GET("/user/:name", func(c *gin.Context) {
-		name := c.Param("name")
-		c.String(http.StatusOK, "hello, %s", name)
+	// add '/hello' view function with handle function
+	engine.GET("/hello", func(context *gin.Context) {
+		fmt.Println("request path: ", context.FullPath())
+		context.Writer.Write([]byte("hello, gin\n"))
 	})
 
-	// 路由分组
-	v1 := engine.Group("/v1")
-	{
-		v1.POST("/login", WebRoot)
-		v1.POST("/logout", WebRoot)
+	// run server with specific port
+	if err := engine.Run(":9090"); err != nil {
+		log.Fatal(err.Error())
 	}
-
-
-
-	// 绑定端口，默认8080
-	_ = engine.Run(":9205")
-
 }
 
-func WebRoot(context *gin.Context) {
-	// 获取查询参数
-	context.DefaultQuery("firstname", "Guest")
-	context.Query("name")
-	// 获取表单数据
-	context.DefaultPostForm("msg", "something")
-	context.PostForm("message")
-
-	// 获取文件
-	file, _ := context.FormFile("file")
-	log.Println(file.Filename)
-
-	context.String(http.StatusOK, "hello, world")
-}
-
-// path中的参数
-func main2() {
-
-}
